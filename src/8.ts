5a483ecc7a09fefffa0d114cfb3d3a11c0be80d0
pontestizil422 <pontestizil422@gmail.com>
2025-03-13 20:44:34 +0000

    Add 8.ts

---
+++ src/8.ts
@@ -0,0 +1,14 @@
+const randomTsCode = `
+interface User {
+  name: string;
+  age: number;
+}
+
+function getUser(): User {
+  return {
+    name: "John Doe",
+    age: 30,
+  };
+}
+`;
+console.log(randomTsCode);
