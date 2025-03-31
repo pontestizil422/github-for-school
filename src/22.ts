58792606e958048c40990cf24a5946abab9c0a8c
pontestizil422 <pontestizil422@gmail.com>
2025-03-31 07:59:16 +0000

    Add 22.ts

---
+++ src/22.ts
@@ -0,0 +1,19 @@
+function fibonacci(n: number): number {
+  if (n <= 1) return n;
+  let a = 0, b = 1;
+  for (let i = 2; i <= n; i++) {
+    [a, b] = [b, a + b];
+  }
+  return b;
+}
+
+function primeNumbers(limit: number): number[] {
+  const isPrime = (num: number): boolean => {
+    if (num < 2) return false;
+    for (let i = 2; i <= Math.sqrt(num); i++) {
+      if (num % i === 0) return false;
+    }
+    return true;
+  };
+
+  return limit
